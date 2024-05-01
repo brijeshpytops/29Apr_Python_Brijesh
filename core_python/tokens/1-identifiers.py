d6725b758a49ccc2e11564bb12392bb71d84c035
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-05-01 15:32:27 +0530

    tokens, identifiers, variables and keywords

---
+++ core_python/tokens/1-identifiers.py
@@ -0,0 +1,7 @@
+# 1] [a-z, A-Z, 0-9, _] - age, Level1_
+# 2] you can not start with digit - 1level, level_1
+# 3] you can not you white-space - first name, first_name
+# 4] identifiers must be unique - x = 10, def x()
+# 5] you can not use special symbols  - @hello
+# 6] you can not use keywords as an identifiers - for = 20
+# 7] python is case sensitive - firstName, FirstName, FirstNAME
---
+++ core_python/tokens/2-variable.py
@@ -0,0 +1,35 @@
+"""
+syntax: 
+var_name = value
+"""
+
+# int x = 10
+
+# x = 10
+
+# x = 10
+# 
+# print(x)
+
+# x, y, z = 10, 20, 30
+# print(x, y, z)
+
+
+# age = input("Enter your age: ")
+# print(age)
+# weight = input("Enter your weight: ")
+# print(weight)
+
+# age, weight = input("Enter your age and weight seperated by comma: ").split(',')
+# print(age, weight)
+
+
+# x = y = 10
+# print(x, y)
+
+
+# x = 10
+# y = 20
+# 
+# print(id(x))
+# print(id(y))
