40267cef3eae929b69796774f9a95c00c487018c
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-05-10 14:55:48 +0530

    while loop

---
+++ core_python/loop/for.py
@@ -0,0 +1,6 @@
+# for ch in "Python":
+#     print(ch.upper())
+
+# nums = [1,2,3,4,5]
+# for num in nums:
+#     print(num * num * num)
---
+++ core_python/loop/while.py
@@ -0,0 +1,41 @@
+# table = 5
+# start = 1
+# end = 10
+# 
+# while(start <= end):
+#     print(table, "*" , start, "=", start * table)
+#     start+=1
+
+# while (1):
+#     name = input("Enter name : ")
+#     print(name)
+
+# while(1):
+#     age = int(input("Enter age: "))
+
+# while(1):
+#     age = int(input("Enter age: "))
+# 
+#     if age >= 18:
+#         weight = int(input("Enter weight: "))
+#         if (weight >= 50):
+#             print("You can donate")
+#         else:
+#             print("You can not donate.")
+#     else:
+#         print("You can not donate.")
+
+# start = 1
+# end = 10
+# 
+# while(start <= end):
+#     print(start * start)
+#     start+=1.
+
+
+# start = 1
+# end = 10
+# 
+# while(start <= end):
+#     print(start * start * start)
+#     start+=1
