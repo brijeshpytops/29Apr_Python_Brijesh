372f80359a1765371a99dc254ab76b9f1c0d10cc
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-05-15 15:21:47 +0530

    for patterns

--- core_python/loop/for.py
+++ core_python/loop/for.py
@@ -4,3 +4,129 @@
 # nums = [1,2,3,4,5]
 # for num in nums:
 #     print(num * num * num)
+
+# for num in range(1,11):
+#     print(num)
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num+1):
+#         print("* ", end='')
+#     print()
+
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print("* ", end='')
+#     print()
+
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num+2-row):
+#         print("* ", end='')
+#     print()
+
+
+#         *
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num+1-row):
+#         print("  ", end='')
+#     for col in range(1, row+1):
+#         print("* ", end='')
+#     print()
+
+
+# * * * * *
+#   * * * *
+#     * * *
+#       * *
+#         *
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, row):
+#         print("  ", end='')
+#     for col in range(1, num+2-row):
+#         print("* ", end='')
+#     print()
+
+#         *
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num+1-row):
+#         print("  ", end='')
+#     for col in range(1, row+1):
+#         print("* ", end='')
+#     for col in range(1, row):
+#         print("* ", end='')
+#     print()
+
+
+# * * * * * * * * *
+#   * * * * * * *
+#     * * * * *
+#       * * *
+#         *
+
+#         *
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+#   * * * * * * *
+#     * * * * *
+#       * * *
+#         *
+
+# num = 5
+# for row in range(1, num):
+#     for col in range(1, num+1-row):
+#         print("  ", end='')
+#     for col in range(1, row+1):
+#         print("* ", end='')
+#     for col in range(1, row):
+#         print("* ", end='')
+#     print()
+# for row in range(1, num+1):
+#     for col in range(1, row):
+#         print("  ", end='')
+#     for col in range(1, num+2-row):
+#         print("* ", end='')
+#     for col in range(1, num+1-row):
+#         print("* ", end='')
+#     print()
+# 
+
+
+# 
+# num = 10
+# for row in range(1, num+1):
+#     print("* "*(num-row-1))
---
+++ core_python/loop/range.py
@@ -0,0 +1,8 @@
+# syntax : 
+
+# range(start, end+1, step+1)
+
+# print(list(range(1,11, 3)))
+
+
+# print(list(range(5, 0, -1)))
